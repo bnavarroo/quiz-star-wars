1a613ead573d3502ec6cd3d8c220af09ebc98cc9
Bruno Navarro <62071446+bnavarroo@users.noreply.github.com>
2022-03-02 13:54:17 -0300

    feat: loader na home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,8 @@
+import { useState } from 'react';
 import type { NextPage } from 'next';
 import Link from 'next/link';
+import PageLoader from '@shared/components/page-loader';
+import showPageLoader from '@modules/pages/home/home.helpers';
 import {
   Container,
   Wrapper,
@@ -10,33 +13,38 @@ import {
   Rules,
 } from '@modules/pages/home/home.styles';
 
-const Home: NextPage = () => (
-  <Container>
-    <Wrapper>
-      <Logo />
-      <Title>StarQuiz</Title>
-      <Subtitle>
-        Teste seus conhecimentos e prove que você é mestre (ou não né rsrs)
-        quando o assunto é StarWars!
-      </Subtitle>
-      <Rules>
-        <b>Instruções:</b> <br />
-        <br />
-        - Você terá dois minutos para acertar o maior número de personagens;
-        <br />
-        <br />
-        - Para cada acerto sem ajuda você recebe 10 pontos. Quando a ajuda é
-        acionada o valor da pontuação cai para 5 pontos;
-        <br />
-        <br />- A resposta pode ser enviada quantas vezes você quiser, porém o
-        que vai contar é sempre a última resposta enviada antes do término do
-        tempo.
-      </Rules>
-      <Link href="/game">
-        <Button>Iniciar o Quiz</Button>
-      </Link>
-    </Wrapper>
-  </Container>
-);
+const Home: NextPage = () => {
+  const [loading, setLoading] = useState<boolean>(false);
+
+  return (
+    <Container>
+      <Wrapper>
+        <Logo />
+        <Title>StarQuiz</Title>
+        <Subtitle>
+          Teste seus conhecimentos e prove que você é mestre (ou não né rsrs)
+          quando o assunto é StarWars!
+        </Subtitle>
+        <Rules>
+          <b>Instruções:</b> <br />
+          <br />
+          - Você terá dois minutos para acertar o maior número de personagens;
+          <br />
+          <br />
+          - Para cada acerto sem ajuda você recebe 10 pontos. Quando a ajuda é
+          acionada o valor da pontuação cai para 5 pontos;
+          <br />
+          <br />- A resposta pode ser enviada quantas vezes você quiser, porém o
+          que vai contar é sempre a última resposta enviada antes do término do
+          tempo.
+        </Rules>
+        <Link href="/game">
+          <Button onClick={showPageLoader(setLoading)}>Iniciar o Quiz</Button>
+        </Link>
+        <PageLoader isVisible={loading} />
+      </Wrapper>
+    </Container>
+  );
+};
 
 export default Home;
